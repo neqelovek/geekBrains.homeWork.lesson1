246fa395aaaf8779e28100d5d03ee1f0ee377385
neqelovek <neqelovek@gmail.com>
2021-10-13 22:47:42 +0300

    start 2

--- src/main/java/geekbrains/lesson1/Main.java
+++ src/main/java/geekbrains/lesson1/Main.java
@@ -1,7 +1,12 @@
 package geekbrains.lesson1;
 
 public class Main {
-    static void main(String [] args){
+     public static void main(String[] args){
+        System.out.println(doTask1(12,45));
+
+    }
+    static int doTask1(int a, int b){
+        return a + b;
 
     }
 
